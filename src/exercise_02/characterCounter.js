1b2222e6633df87cb0237343331f88dfd430c863
Shen Liao <shen.liao@thoughtworks.com>
2020-07-31 06:48:50 +0800

    Finish Exercise-02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,17 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === undefined || string === '' || string === null) {
+    return 0;
+  } if (prediction === undefined) {
+    return string.length;
+  }
+  let length = 0;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < string.length; i++) {
+    if (prediction(string.charAt(i)) === true) {
+      length += 1;
+    }
+  }
+  return length;
 }
